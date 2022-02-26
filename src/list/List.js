fee2f986b77dd2a1ba5d08331c2a7af3432fa555
dotechan <this.is.itoooo.0120@gmail.com>
2022-02-26 22:54:56 +0900

    ListItemにkeyを設定

--- src/list/List.js
+++ src/list/List.js
@@ -3,11 +3,7 @@ import React from "react";
 import "./List.css";
 
 function ListItem({ item }) {
-  return (
-    <li key={item.id} className="li">
-      {item.name}
-    </li>
-  );
+  return <li className="li">{item.name}</li>;
 }
 
 const MemoizedListItem = React.memo(ListItem);
@@ -16,7 +12,7 @@ function List({ items }) {
   return (
     <ul className="ul">
       {items.map((item) => (
-        <MemoizedListItem item={item} />
+        <MemoizedListItem key={item.id} item={item} />
       ))}
     </ul>
   );
